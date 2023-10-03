27f627f9d95495d71dc76f2b684d937b80c2eba4
Oliver <olilinares10@gmail.com>
2023-10-03 09:43:55 +0200

    Redirect to new web

--- src/App.js
+++ src/App.js
@@ -1,53 +1,10 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
-import Home from './pages/Home';
-import List from './pages/List';
-import NotFoundPage from './pages/NotFoundPage';
-import ItemList from "./pages/ItemList";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 
 function App() {
-  const [projectList, setProjectList] = useState();
-  const [tutorialList, setTutorialList] = useState();
-  const [tutorialsIndex, setTutorialsIndex] = useState(0);
-  const [projectsIndex, setProjectsIndex] = useState(0);
-
   useEffect(() => {
-    fetch("https://weboliver.fly.dev/entries/projects/" + projectsIndex + "/10")
-      .then((res) => res.json())
-      .then((json) => setProjectList(json))
-  }, [projectsIndex])
-
-  useEffect(() => {
-    fetch("https://weboliver.fly.dev/entries/tutorials/" + tutorialsIndex + "/10")
-      .then((res) => res.json())
-      .then((json) => setTutorialList(json))
-  }, [tutorialsIndex])
-
-  function changePage(index, page) {
-    console.log(index + " | " + page);
-    if(page === "tutorials") {
-      setTutorialsIndex(index * 10)
-    }
-    else if (page === "projects") {
-      setProjectsIndex(index * 10)
-    }
-  }
-
-
-  return (
-    <HashRouter>
-      <Routes>
-        <Route path='/'>
-          <Route index element={<Home />} />
-          <Route path='tutorials' element={<List list={tutorialList} page={"Tutorials"} changePage={(index, page) => changePage(index, page)} />} />
-          <Route path='tutorials/:id' element={<ItemList list={tutorialList} />} />
-          <Route path='projects' element={<List list={projectList} page={"Projects"} changePage={(index, page) => changePage(index, page)} />} />
-          <Route path='projects/:id' element={<ItemList list={projectList} />} />
-          <Route path='*' element={<NotFoundPage />} />
-        </Route>
-      </Routes>
-    </HashRouter>
-  );
+    window.location.href = "https://www.lincol.es";
+  }, []);
+  return;
 }
 
 export default App;
